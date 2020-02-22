e376039799582510446306eb6be9868f480213ad
dimathe47 <dimathe47@yandex.ru>
2020-02-22 18:15:19 +0300

    use cfg.offset_x and cfg.offset_y if provided in process_pic
    Former-commit-id: 4389c630ef06c74ffce7dda7285a92fc6129affa

--- src/main/python/slice/generator/image/config_patch_image_generator.py
+++ src/main/python/slice/generator/image/config_patch_image_generator.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from shapely.affinity import translate
 
 from slice.annotation_shapely_utils import load_annotation_geoms, create_zlayers_rtrees
 from slice.generator.image.annotations_patch_image_generator import AnnotationsPatchImageGenerator
@@ -22,6 +23,7 @@ def process_pic(patch_geometries: PatchGeometryIterable, cfg: PatchImageConfig) 
     annotations_path = cfg.annotations_path
     annotation_geoms = load_annotation_geoms(annotations_path) if annotations_path else []
     zlayers_rtrees = create_zlayers_rtrees(annotation_geoms)
-    # TODO wrap pig to adjust offset if not None
+    if cfg.offset_x and cfg.offset_y:
+        patch_geometries = map(lambda p: translate(p, cfg.offset_x, cfg.offset_y), patch_geometries)
     patch_images = AnnotationsPatchImageGenerator(cfg.slide_path, cfg.level, cfg.rescale_result_image, zlayers_rtrees).create(patch_geometries)
     return patch_images

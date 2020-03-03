02173f5f1023cdb92769788ff93395efa63730b5
dimathe47 <dimathe47@yandex.ru>
2020-03-04 01:07:57 +0300

    add common_urllib

---
+++ src/main/python/common_urllib/core.py
@@ -0,0 +1,17 @@
+import urllib.request
+from shutil import copyfileobj
+
+
+def load_gdrive_file(file_id: str, file_path: str) -> None:
+    url = f'https://drive.google.com/uc?export=downlod&id={file_id}'
+    with urllib.request.urlopen(url) as resp, open(file_path, 'wb') as f:
+        cookie = resp.info()['Set-Cookie']
+        if 'download_warning' in cookie:
+            confirm = cookie[cookie.find(file_id) + len(file_id) + 1:cookie.find(';')]
+            url = url + f"&confirm={confirm}"
+            req = urllib.request.Request(url)
+            req.add_header('cookie', cookie)
+            with urllib.request.urlopen(req) as resp:
+                copyfileobj(resp, f)
+        else:
+            copyfileobj(resp, f)
